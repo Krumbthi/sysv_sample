ff18e40f74a416d6fa4796d21064a393f4fb8680
Krumbthi <sourcedev@posteo.org>
2019-05-17 11:00:53 +0200

    refactoring

--- server_shm_file.py
+++ server_shm_file.py
@@ -6,14 +6,16 @@ import sys
 import hashlib
 
 # 3rd party modules
-import sysv_ipc
+#import sysv_ipc
 import time
 import logging
 
-import globaltimer
+#import globaltimer
+from multiprocessing import Process
 
 # Utils for this demo
 import utils
+import json
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -22,13 +24,19 @@ import utils
 logging.basicConfig(format='%(asctime)s %(name)s %(message)s', level=logging.DEBUG)
 
 PY_MAJOR_VERSION = sys.version_info[0]
-GlobTimer = globaltimer.GlobalTimer()
+#GlobTimer = globaltimer.GlobalTimer()
 
-class Server(object):
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Class
+# ----------------------------------------------------------------------------------------------------------------------
+class Server(Process):
     def __init__(self):
+        Process.__init__(self)
         self.Logger = logging.getLogger(__class__.__name__)
         self.State = "IDLE"
         self.Cntr = 0
+        self.Running = True
         self.Params = utils.read_params()
         self.CreateFiles()
 
@@ -62,91 +70,95 @@ class Server(object):
         self.Logger.debug("Setup done")
         
     def DispatchMsg(self, s):
-        #msg = s.encode()
-
-        # try:
-        #     assert(msg == hashlib.md5(what_i_wrote).hexdigest())
-        # except AssertionError:
-        #     raise AssertionError("Shared memory corruption after %d iterations." % i)
         if len(s) > 1:
+            self.Logger.info(s)
+            js = json.loads(s)
+            #s = s.split(":")[1].lstrip()
             #self.Logger.info(s)
-            s = s.split(":")[1].lstrip()
-            #self.Logger.info(s)
-            self.State = s
+            self.State = js["State"]
 
     def WriteWithLock(self, file_name, msg):
         self.Logger.debug(msg)
 
-        self.Semaphore.acquire()
+        #self.Semaphore.acquire()
         with open(file_name, "r+") as f:
             f.write("%s" % msg)
-        self.Semaphore.release()
+        #self.Semaphore.release()
 
-    def Process(self):
-        self.Semaphore.release()
-        self.Semaphore.acquire()
+    def run(self):
+        
+        while self.Running:
+            #self.Semaphore.release()
+            #self.Semaphore.acquire()
 
-        with open(os.path.normpath(self.Params["CMD_FILE"]), "r+") as f:
-            fd = f.fileno()
-            flag = fcntl.fcntl(fd, fcntl.F_GETFL)
-            fcntl.fcntl(fd, fcntl.F_SETFL, flag | os.O_NONBLOCK)
-            flag = fcntl.fcntl(fd, fcntl.F_GETFL)
-            
-            #if flag & os.O_NONBLOCK:
-            #    print("O_NONBLOCK!!")
-            
-            res = f.readline()
+            with open(os.path.normpath(self.Params["CMD_FILE"]), "r+") as f:
+                fd = f.fileno()
+                flag = fcntl.fcntl(fd, fcntl.F_GETFL)
+                fcntl.fcntl(fd, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+                flag = fcntl.fcntl(fd, fcntl.F_GETFL)
+                
+                #if flag & os.O_NONBLOCK:
+                #    print("O_NONBLOCK!!")
+                
+                res = f.readline()
 
-        self.DispatchMsg(res)
+            self.DispatchMsg(res)
 
-        self.Semaphore.release()
+            #self.Semaphore.release()
 
-        if self.State == "IDLE":
-            self.Logger.debug("State: IDLE")
-            self.WriteWithLock(os.path.normpath(self.Params["RES_FILE"]), "Counter: %d" % self.Cntr)
-            #self.RespFile.write("Counter: %d" % self.Cntr)
-        elif self.State == "INCR":
-            self.Logger.debug("State: INCR")
-            self.Cntr += 1
-            self.WriteWithLock(os.path.normpath(self.Params["RES_FILE"]), "Counter: %d" % self.Cntr)
-            #self.RespFile.write("Counter: %d" % self.Cntr)
-        elif self.State == "DECR":
-            self.Logger.debug("State: DECR")
-            self.Cntr -= 1
-            self.WriteWithLock(os.path.normpath(self.Params["RES_FILE"]), "Counter: %d" % self.Cntr)
-            #self.RespFile.write("Counter: %d" % self.Cntr)
-        elif self.State == "QUIT":
-            self.Logger.debug("State: QUIT")
-            self.Logger.debug("Final release of the semaphore followed by a 5 second pause")
-            self.Semaphore.release()
-            time.sleep(5)
-            # ...before beginning to wait until it is free again.
-            self.Logger.debug("Final acquisition of the semaphore")
-            self.Semaphore.acquire()
+            if self.State == "IDLE":
+                self.Logger.debug("State: IDLE")
+                self.WriteWithLock(os.path.normpath(self.Params["RES_FILE"]), "Counter: %d" % self.Cntr)
+                #self.RespFile.write("Counter: %d" % self.Cntr)
+            elif self.State == "INCR":
+                self.Logger.debug("State: INCR")
+                self.Cntr += 1
+                self.WriteWithLock(os.path.normpath(self.Params["RES_FILE"]), "Counter: %d" % self.Cntr)
+                #self.RespFile.write("Counter: %d" % self.Cntr)
+            elif self.State == "DECR":
+                self.Logger.debug("State: DECR")
+                self.Cntr -= 1
+                self.WriteWithLock(os.path.normpath(self.Params["RES_FILE"]), "Counter: %d" % self.Cntr)
+                #self.RespFile.write("Counter: %d" % self.Cntr)
+            elif self.State == "QUIT":
+                self.Logger.debug("State: QUIT")
+                self.Logger.debug("Final release of the semaphore followed by a 5 second pause")
+                #self.Semaphore.release()
+                time.sleep(5)
+                # ...before beginning to wait until it is free again.
+                self.Logger.debug("Final acquisition of the semaphore")
+                #self.Semaphore.acquire()
 
-            self.Logger.debug("Destroying semaphore and shared memory")
-            
-            # TODO: remove file from /dev/shm
-            sysv_ipc.remove_semaphore(self.Semaphore.id)
-        else:
-            self.Logger.debug("Wrong State")
+                self.Logger.debug("Destroying semaphore and shared memory")
+                
+                # TODO: remove file from /dev/shm
+                #sysv_ipc.remove_semaphore(self.Semaphore.id)
+                self.Cleanup()
+                self.Running = False
+            else:
+                self.Logger.debug("Wrong State")
+        # finally exit Process
+        return
+
         
     def Cleanup(self):
         self.Logger.debug("Cleanup")
         # TODO: remove file from /dev/shm
-        sysv_ipc.remove_semaphore(self.Semaphore.id)
+        #sysv_ipc.remove_semaphore(self.Semaphore.id)
+        
 
 
-def main():
-    serv = Server()
-    serv.Setup()
+def main():    
+    sp = Server()
+    #sp.Setup()
 
     try:        
-        GlobTimer.register_callback(serv.Process)
+        #GlobTimer.register_callback(serv.Process)
+        sp.start()
     except KeyboardInterrupt:
-        serv.Cleanup()
-        GlobalTimer.unregister_callback(serv.Process)
+        sp.Running = False
     
+    sp.join()
     sys.exit(0)
         
 
